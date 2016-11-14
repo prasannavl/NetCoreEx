09289de9891d58be19fff008e50667b5107b612f
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-11-14 23:24:42 +0530

    add: SizeS, fix Equals boxing, implement IEquatable

--- NetCoreEx.Geometry/Size.cs
+++ NetCoreEx.Geometry/Size.cs
@@ -1,9 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace NetCoreEx.Geometry
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct Size
+    public struct Size : IEquatable<Size>
     {
         public Size(int width, int height)
         {
@@ -18,7 +19,6 @@ namespace NetCoreEx.Geometry
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
             return obj is Size && this.Equals((Size)obj);
         }
 
@@ -36,6 +36,44 @@ namespace NetCoreEx.Geometry
         }
 
         public static bool operator !=(Size left, Size right)
+        {
+            return !(left == right);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SizeS : IEquatable<SizeS>
+    {
+        public SizeS(short width, short height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool Equals(SizeS other)
+        {
+            return (this.Width == other.Width) && (this.Height == other.Height);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SizeS && this.Equals((SizeS)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked { return (this.Width * 397) ^ this.Height; }
+        }
+
+        public short Width;
+        public short Height;
+
+        public static bool operator ==(SizeS left, SizeS right)
+        {
+            return (left.Width == right.Width) && (left.Height == right.Height);
+        }
+
+        public static bool operator !=(SizeS left, SizeS right)
         {
             return !(left == right);
         }
